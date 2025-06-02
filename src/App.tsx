7337aa5229e6f6adddcfc5d32dd3ad1bfe0167d6
hayshin <hayshinbj@gmail.com>
2025-06-02 17:59:37 +0500

    added some features

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,16 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [name, setName] = useState("Anon")
+  const [name, setName] = useState(localStorage.getItem("name") ?? "Anon")
 
   return (
     <>
       <label>
         Username:
-        <input defaultValue={name} onChange={(e) => setName(e.target.value)} />
+        <input defaultValue={name} onChange={(e) => {setName(e.target.value); localStorage.setItem("name", name)}} />
         </label>
-      <h1>{name}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Timer initTime={10} precision={1}/>
+      <h2>{name}</h2>
+      <Timer initTime={3} precision={10} name={name}/>
     </>
   )
 }
--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,29 +1,37 @@
 import { useState, useEffect } from 'react'
 import './Timer.css'
 
-// precision - delta of milliseconds
-function Timer({initTime, precision}: {initTime: number, precision: number}) {
+// precision - delta of milliseconds, doesnt work well using preciision less than 10 ms
+function Timer({ initTime, precision, name }: { initTime: number, precision: number, name: string }) {
   let multiplier = 1000 / precision;
   initTime *= multiplier
 
   const [seconds, setSeconds] = useState(initTime)
-  const [running, setRunning] = useState(false)
+  const [isRunning, setRunning] = useState(false)
+  const [isEnded, setEnded] = useState(false)
 
   useEffect(() => {
     let interval: ReturnType<typeof setInterval>;
-    if (running) {
+    if (isRunning && seconds > 0) {
       interval = setInterval(() => {
-        setSeconds(seconds => seconds-1);
+        setSeconds(seconds => seconds - 1);
       }, precision)
-    }     return () => clearInterval(interval);
-  }, [running])
+    }
+    return () => clearInterval(interval);
+  }, [isRunning])
+
+  useEffect(() => {
+    if (seconds <= 0) { setEnded(true); setRunning(false) }
+  }, [seconds])
 
   return (
     <>
       <div className="card">
-        <h1> {(seconds/multiplier).toFixed(Math.log10(multiplier))} seconds </h1>
-        <button onClick={() => setRunning(true)}>Start </button>
+        <h1>{(seconds / multiplier).toFixed(Math.log10(multiplier))} </h1> <h3> seconds left</h3>
+        <button disabled={isRunning} onClick={() => setRunning(true)}>Start </button>
         <button onClick={() => setRunning(false)}>Pause </button>
+        <button onClick={() => { setRunning(false); setSeconds(initTime); setEnded(false)}}>Reset</button>
+        {isEnded ? <h2>Nice! {name}, You did it</h2> : <h2 />}
       </div>
     </>
   )
