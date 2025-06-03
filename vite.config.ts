6f473a20fe08879f3d97d34b56744fdfb76e7ede
hayshin <hayshinbj@gmail.com>
2025-06-03 06:32:39 +0500

    deploy gh

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   server: {
-    host: "0.0.0.0", // or "0.0.0.0"
-    port: 3000,
-    strictPort: true,
-    cors: true
-  }, plugins: [react()],
+  base: "/timer-react/",
+  plugins: [react()]
+  // server: {
+  //  host: "0.0.0.0", // or "0.0.0.0"
+  //  port: 3000,
+  //  strictPort: true,
+  //  cors: true
+// },
 })
